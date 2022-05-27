478208790d1d96c49ee1605d93e8651c6812eeb6
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-27 17:11:55 +0500

    Printing different types values in JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
 
 function App() {
+  const title = "Welcome to new blog";
+  const likes = 50;
+  const person = { name: "he", male: true };
+  const value = null;
+  const arr = [1, 2, 3, 4];
+  const googleLink = "http://google.com";
+
   return (
     <div className="App">
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Number {likes} times</p>
+
+        <p>Boolean: {String(person.male)}</p>
+        <p>falsy: {String(value)}</p>
+        <p>Array: {arr}</p>
+        <a href={googleLink}>Google link</a>
+        <p>Object: {JSON.stringify(person)}</p>
       </div>
     </div>
   );
