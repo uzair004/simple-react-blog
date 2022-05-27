33c74782e7f1b0d270ecfb11a0e28b2a5236432e
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-27 18:12:36 +0500

    Added external & inline styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,16 @@ const Navbar = () => {
       <h1>Simple Blog</h1>
       <div className="links">
         <a href="/">Home</a>
-        <a href="/create">New Blog</a>
+        <a
+          href="/create"
+          style={{
+            color: "White",
+            backgroundColor: "black",
+            borderRadius: "8px",
+          }}
+        >
+          New Blog
+        </a>
       </div>
     </nav>
   );
