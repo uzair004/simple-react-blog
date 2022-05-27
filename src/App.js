a2d5dd63752eccdb8d4c2078d9c9790cf9da683b
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-27 17:29:33 +0500

    Added simple components
    * navbar
    * home
    components added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 
-function App() {
-  const title = "Welcome to new blog";
-  const likes = 50;
-  const person = { name: "he", male: true };
-  const value = null;
-  const arr = [1, 2, 3, 4];
-  const googleLink = "http://google.com";
+import Navbar from "./Navbar";
+import Home from "./Home";
 
+function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Number {likes} times</p>
-
-        <p>Boolean: {String(person.male)}</p>
-        <p>falsy: {String(value)}</p>
-        <p>Array: {arr}</p>
-        <a href={googleLink}>Google link</a>
-        <p>Object: {JSON.stringify(person)}</p>
+        <Home />
       </div>
     </div>
   );
