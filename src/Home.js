bc6e581bd6ba35bbeb32ceee826ed1ce31fdc48b
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-27 20:44:51 +0500

    Handling click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,22 @@
 const Home = () => {
+  const handleClick = (event) => {
+    console.log("hello world");
+    console.log("event: ", event);
+  };
+
+  const handleClickAgain = (name, event) => {
+    console.log(`Hello ${name}`);
+    console.log("Event:", event);
+  };
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+
+      <button onClick={(e) => handleClickAgain("Uzair", e)}>
+        Click me again
+      </button>
     </div>
   );
 };
