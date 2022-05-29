421daffea345da208613013b288c82016a10bad0
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-30 00:38:47 +0500

    passing props to components

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState([
@@ -10,12 +11,7 @@ const Home = () => {
 
   return (
     <div className="home">
-      {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>author: {blog.author}</p>
-        </div>
-      ))}
+      <BlogList blogs={blogs} title="All Blogs!" />
     </div>
   );
 };
