3e711906401158f60af0cd2a10b85467f4f0030d
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-30 00:55:02 +0500

    Reusing component

--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,16 @@ const Home = () => {
     { id: 2, title: "blog 2", body: "lorem ipsum...", author: "Khan" },
     { id: 3, title: "blog 3", body: "lorem ipsum...", author: "John" },
     { id: 4, title: "blog 4", body: "lorem ipsum...", author: "Doe" },
+    { id: 5, title: "blog 5", body: "lorem ipsum...", author: "Uzair" },
   ]);
 
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Blogs!" />
+      <BlogList
+        blogs={blogs.filter((blog) => blog.author === "Uzair")}
+        title="Uzair Blogs!"
+      />
     </div>
   );
 };
