aaea047527f2ff6fb0b6e9b569d6448ff86fca34
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-31 16:37:36 +0500

    Custom Hook

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
-
   const [name, setName] = useState("Uzair");
-
-  useEffect(() => {
-    fetch("http://localhost:8000/blogsss")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error("Fetching resources failed!");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setIsLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message);
-        setIsLoading(false);
-      });
-  }, []);
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
---
+++ src/useFetch.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Fetching resources failed!");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
+  }, [url]);
+
+  return { data, isLoading, error };
+};
+
+export default useFetch;
