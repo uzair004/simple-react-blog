cc1f34152ecc4781f0a18e671d611a8c4cb6fe04
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-05-31 18:42:06 +0500

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Create from "./Create";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route path="/create" element={<Create />}></Route>
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>Simple Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a
-          href="/create"
-          style={{
-            color: "White",
-            backgroundColor: "black",
-            borderRadius: "8px",
-          }}
-        >
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
