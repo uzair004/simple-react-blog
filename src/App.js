c3b79a59ac900c77488a5920608b9287ce0b1e9f
Muhammad Uzair <34101864+uzair004@users.noreply.github.com>
2022-06-02 22:36:48 +0500

    Not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
             <Route exact path="/" element={<Home />}></Route>
             <Route path="/create" element={<Create />}></Route>
             <Route path="/blogs/:id" element={<BlogDetails />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
           </Routes>
         </div>
       </div>
